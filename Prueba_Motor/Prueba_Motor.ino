ee08939c867e59bb72b6938ca623e304bf7ffcbf
Sergio Guillen <serguipe28@gmail.com>
2022-02-21 13:44:46 +0100

    Pruebas 2 motores correcto

--- Prueba_Motor/Prueba_Motor.ino
+++ Prueba_Motor/Prueba_Motor.ino
@@ -1,9 +1,11 @@
-// Prueba Shield CNC, Driver, Motores de Pasos y Arduino UNO
-// By: www.dinastiatecnologica.com
+
 //
 #define x_paso 2    // Define el Pin de STEP para Motor de eje X
 #define x_dire 5    // Define el Pin de DIR  para Motor de eje X
 #define x_habi 8    // Define el Pin de ENABLE  para Motor de eje X
+#define y_paso 3
+#define y_dire 8
+#define y_habi 8
 
 
 int retardo = 100;   // Menor numero el giro es mas rapido
@@ -11,12 +13,12 @@ int tiempo = 3000;   // durante cuanto timpo da el giro el motor (vueltas)
 
 void setup() {
 pinMode(x_paso, OUTPUT); pinMode(x_dire, OUTPUT); pinMode(x_habi, OUTPUT);     
-
+pinMode(y_paso, OUTPUT); pinMode(y_dire, OUTPUT); pinMode(y_habi, OUTPUT);
 }    
 
 void loop() {
   giro(x_paso,x_dire,x_habi);
-  
+  giro(y_paso,y_dire,y_habi);
 }
 
 void giro(int paso_,int dire_,int habi_) {
